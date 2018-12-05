c57c6cd87fd20c47848947f8afbfd1a2140cb4de
singletonn <sopranowest@gmail.com>
2018-12-05 14:55:08 +1000

    Инициализация редактора только там где он есть

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,20 @@
 const mailTemplateClass = function () {
     this.initAce = function () {
         const textarea = $('#mailtemplate-template');
-        const editor = ace.edit("mail-template-editor");
-        editor.setTheme("ace/theme/dracula");
-        editor.getSession().setMode({path: "ace/mode/twig", inline: true});
-        editor.getSession().on("change", function () {
-            textarea.val(editor.getSession().getValue());
-        });
-        editor.getSession().setValue(textarea.val());
+        if (textarea.length) {
+            const editor = ace.edit("mail-template-editor");
+            editor.setTheme("ace/theme/dracula");
+            editor.getSession().setMode({path: "ace/mode/twig", inline: true});
+            editor.getSession().on("change", function () {
+                textarea.val(editor.getSession().getValue());
+            });
+            editor.getSession().setValue(textarea.val());
+        }
+    };
+    this.init = function () {
+        this.initAce();
     };
 };
 
 const mailTemplate = new mailTemplateClass();
-mailTemplate.initAce();
+mailTemplate.init();
